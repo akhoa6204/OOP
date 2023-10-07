7ce75629f50f7948b01c3f98415aeb3bc98f914f
akhoa6204 <khoaanh662004@gmail.com>
2023-10-07 16:08:37 +0700

    sadasd

--- c4b5/Program.cs
+++ c4b5/Program.cs
@@ -1,1 +1,73 @@
-﻿
+﻿class DoanhNghiep 
+{
+    public string TenDN,Diachi;
+    public int MST;
+}
+class DanhsachDN
+{
+    public static string [] DSDN = new string [50];
+    public static int size=0;
+    DoanhNghiep DN = new DoanhNghiep();
+    public DanhsachDN()
+    {
+        Array.Resize(ref DSDN, size++);
+        Console.Write("TenDN: ");
+        DN.TenDN = Console.ReadLine();
+        Console.Write("Diachi: ");
+        DN.Diachi = Console.ReadLine();
+        Console.Write("MST: ");
+        DN.MST = int.Parse(Console.ReadLine());
+        DSDN[size]=(DN.TenDN+" "+DN.Diachi+" "+DN.MST);
+        size++;
+        // Array.Resize(ref DSDN,size+1);        
+    }
+}
+class Index    
+{   
+    public string this[int index]
+    {
+        get
+        {
+            string tmp;
+            if (index>=0 && index <=DanhsachDN.size-1)
+            {
+                tmp=DanhsachDN.DSDN[index];
+            }
+            else 
+            {
+                tmp="";
+            }
+            return (tmp);
+        }
+        set
+        {
+            if (index >=0 && index <=DanhsachDN.size-1)
+            {DanhsachDN.DSDN[index]=value;}
+        }
+    }
+    public static void Xuat(string a,string b , int c)
+    {
+        Console.WriteLine(a+" "+b+" "+c);
+    }
+
+}
+class Program
+{
+    static void Main(string [] args)
+    {
+        DanhsachDN DN1= new DanhsachDN();
+        DanhsachDN DN2= new DanhsachDN();
+        DanhsachDN Dn2= new DanhsachDN();
+        Index IndexDN =new Index();
+        Console.Write("MST can tim: ");
+        string MSTfind=Console.ReadLine();
+        for (int i=0; i<DanhsachDN.size-1; i++) 
+        {
+            if (DanhsachDN.DSDN[i].Contains(MSTfind))
+            {
+                Console.WriteLine(DanhsachDN.DSDN[i]);
+            }
+        }
+
+    }
+}
---
+++ c4tl2/Program.cs
@@ -0,0 +1,45 @@
+﻿
+namespace BTLop
+{
+    class Programm
+    {
+        public static void Main(string[] args)
+        {
+            Chitieu t10 = new Chitieu(700,500);
+            t10.Kiemtra();
+        }
+    }
+    class Chitieu
+    {
+        private double Income,Outcome,Balance,Cost;
+        public Chitieu(double Income, double Outcome)
+        {
+            this.Income = Income;
+            this.Outcome = Outcome;
+        }
+        public double BALANCE
+        {
+            get
+            {
+                return Income - Outcome;
+            }
+        }
+        public double COST 
+        {
+            get{return (Outcome/Income)*100;}
+        }
+        public void Kiemtra()
+        {
+            if (BALANCE <= 10 || COST >=90)
+            {
+                Console.WriteLine("Thang nay da tieu het {0}%, con lai {1}$",COST,BALANCE);
+                Console.WriteLine("Het tien roi");
+            }
+            else 
+            {
+                Console.WriteLine("Thang nay da tieu het {0}%, con lai {1}$",COST,BALANCE);
+                Console.WriteLine("Con tien");
+            }
+        }
+    }
+}
