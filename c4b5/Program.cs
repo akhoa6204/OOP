7aac082e3813cd0500fa6bb2acd897c46300d79e
akhoa6204 <khoaanh662004@gmail.com>
2023-10-09 15:26:12 +0700

    assa

--- c4b5/Program.cs
+++ c4b5/Program.cs
@@ -1,55 +1,64 @@
 ﻿class DoanhNghiep 
 {
-    public string TenDN,Diachi;
+    public string TenDN;
+    public string Diachi;
     public int MST;
 }
 class DanhsachDN
 {
-    public static string [] DSDN = new string [50];
     public static int size=0;
     DoanhNghiep DN = new DoanhNghiep();
     public DanhsachDN()
-    {
-        Array.Resize(ref DSDN, size++);
+    {   
+        size++;
+        Array.Resize(ref Index.DSDN, size);
         Console.Write("TenDN: ");
         DN.TenDN = Console.ReadLine();
         Console.Write("Diachi: ");
         DN.Diachi = Console.ReadLine();
         Console.Write("MST: ");
         DN.MST = int.Parse(Console.ReadLine());
-        DSDN[size]=(DN.TenDN+" "+DN.Diachi+" "+DN.MST);
-        size++;
-        // Array.Resize(ref DSDN,size+1);        
-    }
-}
-class Index    
-{   
-    public string this[int index]
-    {
-        get
-        {
-            string tmp;
-            if (index>=0 && index <=DanhsachDN.size-1)
-            {
-                tmp=DanhsachDN.DSDN[index];
-            }
-            else 
-            {
-                tmp="";
-            }
-            return (tmp);
-        }
-        set
-        {
-            if (index >=0 && index <=DanhsachDN.size-1)
-            {DanhsachDN.DSDN[index]=value;}
-        }
+        Index.DSDN[size-1]=DN.TenDN+" "+DN.Diachi+" "+DN.MST;
     }
     public static void Xuat(string a,string b , int c)
     {
         Console.WriteLine(a+" "+b+" "+c);
     }
-
+}
+class Index
+{
+    public static string [] DSDN = new string [DanhsachDN.size];
+    public string this[string tendn]
+    {
+        get
+        {
+            for (int i = 0; i < DanhsachDN.size; i++)
+            {
+                string[] parts = DSDN[i].Split();
+                if (parts[0] == tendn)
+                {
+                    return parts[2];
+                }
+                
+            }
+            return "";
+        }
+    }
+    public string this [int mst]
+    {
+        get
+        {
+            for (int i=0; i<DanhsachDN.size;i++) 
+            {
+                string[] parts = DSDN[i].Split();
+                if(parts[2]==mst.ToString())
+                {
+                    return parts[0]+" "+parts[1]; 
+                }
+            }
+            return "";
+        }
+    }
 }
 class Program
 {
@@ -57,17 +66,21 @@ class Program
     {
         DanhsachDN DN1= new DanhsachDN();
         DanhsachDN DN2= new DanhsachDN();
-        DanhsachDN Dn2= new DanhsachDN();
-        Index IndexDN =new Index();
-        Console.Write("MST can tim: ");
-        string MSTfind=Console.ReadLine();
-        for (int i=0; i<DanhsachDN.size-1; i++) 
-        {
-            if (DanhsachDN.DSDN[i].Contains(MSTfind))
-            {
-                Console.WriteLine(DanhsachDN.DSDN[i]);
-            }
-        }
+        DanhsachDN DN3= new DanhsachDN();
+        DanhsachDN DN4= new DanhsachDN();
+        Index index= new Index();
+        Console.Write("TenDN can tim MST: ");
+        string tpdn= Console.ReadLine();
+        string dn =index[tpdn];
+        if(dn != "" )
+        {Console.WriteLine(dn);} 
+        else {Console.WriteLine($"Doanh nghiep ten {tpdn} khong ton tai");}
+        Console.Write("MST cua dn can tim: ");
+        tpdn =Console.ReadLine();
+        dn=index[int.Parse(tpdn)];  
+        if(dn.ToString() != "" ) 
+        {Console.WriteLine(dn);}  
+        else {Console.WriteLine($"Doanh nghiep co mst {tpdn} khong ton tai");}
 
     }
 }
