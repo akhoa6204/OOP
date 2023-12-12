675edd4958838ef525dbd929252033e5027c5dc9
akhoa6204 <khoaanh662004@gmail.com>
2023-12-12 13:38:52 +0700

    asda

---
+++ c6/c6b3list/Program.cs
@@ -0,0 +1,120 @@
+﻿abstract class Edition
+{
+    public string title;
+    public string author;
+    public Edition (string title, string author)
+    {
+        this.title = title;
+        this.author = author;
+    }
+    abstract public void CompareTo();
+}
+class Book : Edition
+{   
+    public static List<Book> BookList = new List<Book>();
+    public int year;
+    public string publisher;
+    public Book(string title, string author, int year, string publisher) : base(title, author)
+    {
+        this.year = year; 
+        this.publisher = publisher;
+        CompareTo();
+    }
+    public override void CompareTo()
+    {
+        BookList.Add(this);
+        BookList.Sort((s1,s2) => s1.author.CompareTo(s2.author));
+    }
+}
+class Article : Edition
+{ 
+    public static List<Article> ArticleList = new List<Article>();
+    public int year;
+    public string journal;
+    public Article(string title, string author, int year, string journal) : base(title, author)
+    {
+        this.year = year; 
+        this.journal = journal;
+        CompareTo();
+    }
+    public override void CompareTo()
+    {
+        ArticleList.Add(this);
+        ArticleList.Sort((s1,s2) => s1.author.CompareTo(s2.author));
+    }
+}
+class OnlineResoure : Edition
+{   
+    public static List<OnlineResoure> OnlineResoureList = new List<OnlineResoure>();
+    public string link;
+    public string abs;
+    public OnlineResoure(string title, string author, string link, string abs) : base(title, author)
+    {
+        this.link = link;
+        this.abs = abs;
+        CompareTo();
+    } 
+    public override void CompareTo()
+    {
+        OnlineResoureList.Add(this);
+        OnlineResoureList.Sort((s1,s2) => s1.author.CompareTo(s2.author));
+    }
+}
+class programm 
+{
+    public static void Main(string[] args)
+    {
+
+            Edition b1 = new Book("Sun", "banh", 1990, "kim dong");
+            Edition b2 = new Book("Moon", "anh", 1991, "tu do");
+            Edition b3 = new Book("Black", "danh", 1992, "truong doi"); 
+            Edition b4 = new Book("Red", "manh", 1993, "truong hoc");
+            Edition b5 = new Book("Gold", "nanh", 1994, "giao duc");
+            Edition b6 = new OnlineResoure("Fun1", "to huu", "example.com", "quang nam");
+            Edition b7 = new OnlineResoure("Dun2", "dieu lam", "example.com", "quang nam"); 
+            Edition b8 = new OnlineResoure("Aun3", "cong nghia", "example.com", "quang nam"); 
+            Edition b9 = new Article("Sun", "bao ngoc", 1258, "ha noi");
+            Edition b10 = new Article("Bun", "khoa", 1992, "da nang");
+            Edition b11 = new Book("Hun", "banh", 1992, "kim dong");
+            Edition b12 = new OnlineResoure("Asun3", "banh", "example.com", "tphcm");
+            Edition b13 = new Article("Saun", "banh", 1258, "lang son");
+            Console.WriteLine("-----Book-----");
+            foreach (var s in Book.BookList)
+            {
+                Console.WriteLine($"{s.title} {s.author} {s.year} {s.publisher}");
+            }
+            Console.WriteLine("-----Article-----");
+            foreach (var s in Article.ArticleList)
+            {
+                Console.WriteLine($"{s.title} {s.author} {s.year} {s.journal}");
+            }
+            Console.WriteLine("-----OnlineResoure-----");
+            foreach (var s in OnlineResoure.OnlineResoureList)
+            {
+                Console.WriteLine($"{s.title} {s.author} {s.link} {s.abs}");
+            }
+            Console.Write("Tên tác giả ấn phẩm cần tìm: ");
+            string author = Console.ReadLine();
+            foreach (var s in Book.BookList)
+            {
+                if (s.author == author)
+                {
+                    Console.WriteLine($"{s.title} {s.author} {s.year} {s.publisher}");
+                }
+            }
+            foreach (var s in Article.ArticleList)
+            {
+                if (s.author == author)
+                {
+                    Console.WriteLine($"{s.title} {s.author} {s.year} {s.journal}");
+                }
+            }
+            foreach (var s in OnlineResoure.OnlineResoureList)
+            {
+                if (s.author == author)
+                {
+                    Console.WriteLine($"{s.title} {s.author} {s.link} {s.abs}");
+                }
+            }
+    }
+}
---
+++ ontapck/b1/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+
+class PhuongTienGiaoThong 
+{
+    public string hsx, ten;
+    public double nsx, vt;
+
+    public PhuongTienGiaoThong(string hsx, string ten, double nsx, double vt)
+    {
+        this.hsx = hsx; 
+        this.ten = ten;
+        this.nsx = nsx;
+        this.vt = vt;
+    }
+
+    public void Xuat()
+    {
+        Console.WriteLine($"Hãng sản xuất: {hsx}\nTên phương tiện: {ten}\nNhà sản xuất: {nsx}\nVận tốc tối đa: {vt}");
+    }
+}
+
+class OTo : PhuongTienGiaoThong
+{
+    public int socho;
+    public string kieudongco;
+
+    public OTo(string hsx, string ten, double nsx, double vt, int socho, string kieudongco) : base(hsx, ten, nsx, vt)
+    {
+        this.socho = socho;
+        this.kieudongco = kieudongco;
+    }
+
+    public new void Xuat()
+    {
+        base.Xuat();
+        Console.WriteLine($"Số chỗ: {socho}\nKiểu động cơ: {kieudongco}\nVận tốc cơ bản {VTCS()}");
+    }
+
+    public double VTCS()
+    {
+        return vt / socho; 
+    }
+}
+
+class Program 
+{
+    public static void Main(string[] args)
+    {
+        List<OTo> ds = new List<OTo>
+        {
+            new OTo("Vinfast", "kLuxA", 2019, 200, 4, "nặng"),
+            new OTo("Vinfast", "jLuxA", 2019, 220, 4, "trung bình"),
+            new OTo("Vinfast", "hLuxA", 2019, 250, 8, "nhẹ"),
+            new OTo("Vinfast", "gLuxA", 2019, 270, 6, "nhẹ"),
+            new OTo("Vinfast", "fLuxA", 2019, 3200, 4, "nhẹ"),
+            new OTo("Vinfast", "eLuxA", 2019, 1200, 4, "trung bình"),
+            new OTo("Vinfast", "dLuxA", 2019, 1000, 4, "nặng"),
+            new OTo("Vinfast", "cLuxA", 2019, 600, 4, "nhẹ"),
+            new OTo("Vinfast", "bLuxA", 2019, 300, 4, "trung bình"),
+            new OTo("Vinfast", "aLuxA", 2019, 180, 4, "nhẹ")
+        };
+        ds.Sort((oto1, oto2) => oto1.kieudongco.CompareTo(oto2.kieudongco));
+        ds.Sort((oto1, oto2) =>
+        {
+            if (oto1.kieudongco == "nhẹ" && oto2.kieudongco == "nhẹ")
+                return 0;
+            else if (oto1.kieudongco == "nhẹ")
+                return -1;
+            else if (oto2.kieudongco == "nhẹ")
+                return 1;
+            else if (oto1.kieudongco == "trung bình" && oto2.kieudongco == "trung bình")
+                return 0;
+            else if (oto1.kieudongco == "trung bình")
+                return -1;
+            else
+                return 1;
+        });
+
+        foreach (var oto in ds)
+        {
+            oto.Xuat();
+        }
+        foreach (var oto in ds)
+        {
+            Console.WriteLine($"{oto.hsx} {oto.ten} {oto.nsx} {oto.vt} {oto.socho} {oto.kieudongco} {oto.VTCS()}");
+        }
+    }
+}
